6c64d3f470aa081d4a27d0d708356149b27d4cd0
meghdadya <meghdadya@gmail.com>
2018-01-28 12:54:48 +0330

    java server done

--- server/src/admin/ClientServer.java
+++ server/src/admin/ClientServer.java
@@ -7,12 +7,19 @@ import server.MultiThreadChatServerSync;
 //Class to precise who is connected : Client or Server
 public class ClientServer {
 
-	public static void main(String[] args) {
-
-			String[] arguments = new String[] {};
+	
+	public static void main(String [] args){
+		
+		Object[] selectioValues = {"Server"};
+		String initialSection = "Server";
+		
+		Object selection = JOptionPane.showInputDialog(null, "Login as : ", "MyChatApp", JOptionPane.QUESTION_MESSAGE, null, selectioValues, initialSection);
+		if(selection.equals("Server")){
+                   String[] arguments = new String[] {};
 			new MultiThreadChatServerSync();
 			MultiThreadChatServerSync.main(arguments);
-
+		}
+		
 	}
 
 }
